e0eeb4654b2a0d2fb93143ee686fa64c92284bdf
ashis <ashis325@gmail.com>
2021-02-12 23:34:42 +0530

    adding routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import Homepage from './pages/homepage/homepage'
+import {Route,Switch} from 'react-router-dom'
+
+const Hatspage=()=>(
+  <div>
+    <h1>It is hats</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route exact path="/hats" component={Hatspage} />
+      </Switch>
+
     </div>
   );
 }
--- src/component/menu-item/menu-item.jsx
+++ src/component/menu-item/menu-item.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
-
+import {withRouter} from 'react-router-dom'
 import './menu-item.scss';
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
+/* history props getting through app component where component routed,it called props drilling 
+   it is bad practice in react routing.
+
+   withRouter gives history,location,match propertise to routing whole component.
+*/ 
+const MenuItem = ({ title, imageUrl, size, history,match,linkUrl }) => (
+  <div className={`${size} menu-item`} onClick={()=>(history.push(`${match.url}${linkUrl}`))}>
     <div
       className='background-image'
       style={{
@@ -13,8 +18,9 @@ const MenuItem = ({ title, imageUrl, size }) => (
     <div className='content'>
       <h1 className='title'>{title.toUpperCase()}</h1>
       <span className='subtitle'>SHOP NOW</span>
+
     </div>
   </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem) ;
