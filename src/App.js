5ba75c9842e9ed416dd6d089f14c1c87f58e0a94
ashis <ashis325@gmail.com>
2021-02-13 05:40:51 +0530

    shop page and header added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css'
 import Homepage from './pages/homepage/homepage'
 import Shoppage from './pages/shop/shop'
+import Header from './component/header/header'
 
 import {Route,Switch} from 'react-router-dom'
 
@@ -8,6 +9,7 @@ import {Route,Switch} from 'react-router-dom'
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route exact path="/shop" component={Shoppage} />
